9c4403a7a024b88752bb4fb9e4820e07d9a67373
nbacskai <bacskai.n@husky.neu.edu>
2022-04-29 17:29:49 -0400

    stat tracking

--- Assets/Scripts/MainMenuManager.cs
+++ Assets/Scripts/MainMenuManager.cs
@@ -2,12 +2,46 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using TMPro;
 
 public class MainMenuManager : MonoBehaviour
 {
+    public TMP_Text timeText;
+    public TMP_Text scoreText;
+    public TMP_Text winsText;
+
+    void Start()
+    {
+        if (PlayerPrefs.HasKey("time_played"))
+        {
+            timeText.text = PlayerPrefs.GetFloat("time_played").ToString();
+        }
+        else
+        {
+            timeText.text = "0";
+        }
+        if (PlayerPrefs.HasKey("high_score"))
+        {
+            scoreText.text = PlayerPrefs.GetInt("high_score").ToString();
+        }
+        else
+        {
+            scoreText.text = "0";
+        }
+        if (PlayerPrefs.HasKey("game_wins"))
+        {
+            winsText.text = PlayerPrefs.GetInt("game_wins").ToString();
+        }
+        else
+        {
+            winsText.text = "0";
+        }
+    }
     public void StartGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        
     }
 
     void OpenSettings()
