dfc987f7ec97eaac622994ab3337a17b17b48741
hurk99 <hur.k@husky.neu.edu>
2022-04-29 02:55:54 -0400

    Made the settings persistent

--- Assets/Scripts/PauseMenuBehavior.cs
+++ Assets/Scripts/PauseMenuBehavior.cs
@@ -2,13 +2,35 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class PauseMenuBehavior : MonoBehaviour
 {
     public GameObject menu;
     public GameObject reticle;
     public AudioMixer audioMixer;
+    public Slider volumeSlider;
+    public Slider sensitivitySlider;
     public static bool isGamePaused;
+
+    void Start()
+    {
+        if (PlayerPrefs.HasKey("volume"))
+        {
+            float volume = PlayerPrefs.GetFloat("volume");
+            Debug.Log("HasV Key");
+            SetVolume(volume);
+            volumeSlider.value = volume;
+        }
+
+        if (PlayerPrefs.HasKey("sensitivity"))
+        {
+            float sensitivity = PlayerPrefs.GetFloat("sensitivity");
+            Debug.Log("HasS Key");
+            SetSensitivity(sensitivity);
+            sensitivitySlider.value = sensitivity;
+        }
+    }
 
     // Update is called once per frame
     void Update()
@@ -45,10 +67,13 @@ public class PauseMenuBehavior : MonoBehaviour
     public void SetVolume(float volume)
     {
         audioMixer.SetFloat("volume", volume);
+        PlayerPrefs.SetFloat("volume", volume);
     }
 
     public void SetSensitivity(float sensitivity)
     {
+        
         MouseLook.sensitivity = sensitivity;
+        PlayerPrefs.SetFloat("sensitivity", sensitivity);
     }
 }
